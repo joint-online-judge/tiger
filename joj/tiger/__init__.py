f88aa755ac4301b6f81bc9792f3a9deb0e50b0a5
BoYanZh <bomingzh@sjtu.edu.cn>
2021-05-09 22:35:13 +0800

    feat: pass cases status to horse

--- joj/tiger/__init__.py
+++ joj/tiger/__init__.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from typing import Any, Dict
 
 from autograder_sandbox import AutograderSandbox
-from celery import Celery
+from celery import Celery, current_task
 
 celery_app = Celery("tasks", backend="rpc://", broker="pyamqp://localhost//")
 
@@ -16,13 +17,36 @@ celery_app.conf.update(
 
 
 @celery_app.task(name="joj.tiger.compile")
-def compile_task() -> Dict[str, Any]:
-    print("compile_task")
+def compile_task(record_dict: Dict[str, Any]) -> Dict[str, Any]:
+    print("compile_task", record_dict)
+    total_time_ms = 0
+    total_memory_kb = 0
+    total_score = 0
+    judge_at = datetime.utcnow()
     with AutograderSandbox() as sandbox:
-        result = sandbox.run_command(["echo", "1"])
+        for i, case in enumerate(record_dict["cases"]):
+            result = sandbox.run_command(["echo", "1"])
+            score = 100
+            time_ms = int(result.time * 1000)
+            memory_kb = result.memory
+            meta = {
+                "index": i,
+                "status": 1,
+                "score": score,
+                "time_ms": time_ms,
+                "memory_kb": memory_kb,
+                "execute_status": 1,
+                "stdout": result.stdout.read().decode(),
+                "stderr": result.stderr.read().decode(),
+            }
+            current_task.update_state(state="PROGRESS", meta=meta)
+            total_score += score
+            total_time_ms += time_ms
+            total_memory_kb += memory_kb
     return {
-        "time_ms": int(result.time * 1000),
-        "memory_kb": result.memory,
-        "stdout": result.stdout.read().decode(),
-        "stderr": result.stderr.read().decode(),
+        "status": 1,
+        "score": total_score,
+        "time_ms": total_time_ms,
+        "memory_kb": total_memory_kb,
+        "judge_at": judge_at,
     }
