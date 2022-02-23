cd5d15a9742e10035cd8fa5315bb65c1c8b8d1ae
BoYanZh <bomingzh@sjtu.edu.cn>
2022-02-24 01:57:14 +0800

    feat: runner with timeout control

--- runner/main.go
+++ runner/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/containerd/cgroups"
 	"github.com/opencontainers/runtime-spec/specs-go"
@@ -29,6 +30,9 @@ func main() {
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
+	done := make(chan bool, 1)
+	timeout_ms := 1000 * time.Millisecond
+	start := time.Now()
 	err = cmd.Start()
 	if err != nil {
 		panic(err)
@@ -37,11 +41,20 @@ func main() {
 	if err := control.Add(cgroups.Process{Pid: pid}); err != nil {
 		panic(err)
 	}
-	go func() {
+	go func(done chan bool) {
 		if err = cmd.Wait(); err != nil {
 			panic(err)
 		}
+		fmt.Printf("done in %v\n", time.Since(start))
+		done <- true
+	}(done)
+	select {
+	case <-done:
 		fmt.Printf("stdout: %s\n", stdout.String())
 		fmt.Printf("stderr: %s\n", stderr.String())
-	}()
+	case <-time.After(timeout_ms):
+		fmt.Printf("timeout in %v\n", time.Since(start))
+		cmd.Process.Kill()
+		os.Exit(1)
+	}
 }
