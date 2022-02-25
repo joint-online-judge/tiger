4c0b397612e23463cc8d75432838a9bf75066b99
BoYanZh <bomingzh@sjtu.edu.cn>
2022-02-26 02:50:58 +0800

    feat: print result msgpack

--- runner/go.mod
+++ runner/go.mod
@@ -4,10 +4,15 @@ go 1.17
 
 require (
 	github.com/containerd/cgroups v1.0.3 // direct
+	github.com/opencontainers/runtime-spec v1.0.2 // direct
+)
+
+require (
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/opencontainers/runtime-spec v1.0.2 // direct
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- runner/main.go
+++ runner/main.go
@@ -5,14 +5,35 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/containerd/cgroups"
 	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
+type CompletedCommand struct {
+	ReturnCode int
+	Stdout     []byte
+	Stderr     []byte
+	TimedOut   bool
+	Time       uint64
+	Memory     uint64
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: main <command>")
+		os.Exit(1)
+	}
+	u, err := user.Lookup(os.Getenv("SUDO_USER"))
+	if err != nil {
+		panic(err)
+	}
+	timeoutMs := 1000
 	control, err := cgroups.New(
 		cgroups.V1,
 		cgroups.StaticPath("/joj.tiger"),
@@ -22,15 +43,12 @@ func main() {
 		panic(err)
 	}
 	defer control.Delete()
-	if len(os.Args) < 2 {
-		fmt.Println("usage: main <command>")
-		os.Exit(1)
-	}
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{}
 	// run command as non-root
-	// TODO: better way to choose Uid & Gid
-	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: 1000, Gid: 1000}
+	uid, _ := strconv.ParseUint(u.Uid, 10, 32)
+	gid, _ := strconv.ParseUint(u.Gid, 10, 32)
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
@@ -53,22 +71,35 @@ func main() {
 			exit_code <- 0
 		}
 	}(exitCode)
-	timeoutLimit := 1000 * time.Millisecond
+	timeoutLimit := time.Duration(timeoutMs) * time.Millisecond
 	timedOut := false
 	select {
 	case returnCode = <-exitCode:
-		fmt.Fprintf(os.Stderr, "done in %v\n", time.Since(start))
+		fmt.Fprintf(os.Stderr, "status: done in %v\n", time.Since(start))
 	case <-time.After(timeoutLimit):
-		fmt.Fprintf(os.Stderr, "timeout in %v\n", time.Since(start))
+		fmt.Fprintf(os.Stderr, "status: timeout in %v\n", time.Since(start))
 		cmd.Process.Kill()
 		returnCode = <-exitCode
 		timedOut = true
 	}
 	stats, _ := control.Stat(cgroups.IgnoreNotExist)
-	fmt.Printf("return_code: %d\n", returnCode)
-	fmt.Printf("time: %d\n", stats.CPU.Usage.Total)
-	fmt.Printf("memory: %d\n", stats.Memory.Usage.Max) // Memory.Usage.Max = 0 when killed
-	fmt.Printf("stdout: %s\n", stdout.String())
-	fmt.Printf("stderr: %s\n", stderr.String())
-	fmt.Printf("timed_out: %v\n", timedOut)
+	fmt.Fprintf(os.Stderr, "return_code: %d\n", returnCode)
+	fmt.Fprintf(os.Stderr, "time: %d\n", stats.CPU.Usage.Total)
+	fmt.Fprintf(os.Stderr, "memory: %d\n", stats.Memory.Usage.Max) // Memory.Usage.Max = 0 when killed
+	fmt.Fprintf(os.Stderr, "stdout: %s\n", stdout.String())
+	fmt.Fprintf(os.Stderr, "stderr: %s\n", stderr.String())
+	fmt.Fprintf(os.Stderr, "timed_out: %v\n", timedOut)
+	completedCommand := CompletedCommand{
+		ReturnCode: returnCode,
+		Stdout:     stdout.Bytes(),
+		Stderr:     stderr.Bytes(),
+		TimedOut:   timedOut,
+		Time:       stats.CPU.Usage.Total,
+		Memory:     stats.Memory.Usage.Max,
+	}
+	b, err := msgpack.Marshal(&completedCommand)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", string(b))
 }
