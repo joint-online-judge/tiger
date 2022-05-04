a2746eab08677507231b006dc8b99aa151f81e2e
BoYanZh <bomingzh@sjtu.edu.cn>
2022-05-04 14:10:14 +0800

    feat: add startup_event

--- joj/tiger/app.py
+++ joj/tiger/app.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import platform
 from typing import Any, Dict
@@ -7,10 +8,12 @@ from celery.signals import setup_logging
 from loguru import logger
 from pydantic_universal_settings import init_settings
 from pydantic_universal_settings.cli import async_command
+from tenacity import RetryError
 
 from joj.tiger.config import AllSettings
 from joj.tiger.task import TigerTask
 from joj.tiger.toolchains import get_toolchains_config
+from joj.tiger.utils.retry import retry_init
 
 
 class InterceptHandler(logging.Handler):
@@ -39,7 +42,6 @@ def setup_celery_logging(*args: Any, **kwargs: Any) -> None:
 
 settings = init_settings(AllSettings, overwrite=False)
 app = Celery("tasks", backend=settings.backend_url, broker=settings.broker_url)
-logger.info(app.control.inspect().active())
 
 # initialize toolchains supported
 toolchains_config = get_toolchains_config()
@@ -59,9 +61,6 @@ app.conf.update(
 )
 
 
-# logger.info(app.conf)
-# asyncio.run(get_access_token())
-
 # app.user_options["preload"].add(Option("-j", dest="jwt"))
 
 
@@ -74,56 +73,8 @@ app.conf.update(
 #
 #
 # app.steps["worker"].add(ConfigBootstep)
-#
-#
-# async def compile_task_impl(record_dict: Dict[str, Any], base_url: str) -> None:
-#     HEADERS = current_app.conf.get("HEADERS")
-#     print("compile_task", record_dict, base_url)
-#     record_url = base_url + "/http"
-#     case_url = base_url + "/cases/http"
-#     total_time_ms = 0
-#     total_memory_kb = 0
-#     total_score = 0
-#     judge_at = datetime.utcnow()
-#     async with aiohttp.ClientSession() as session:
-#         with AutograderSandbox() as sandbox:
-#             for i, case in enumerate(record_dict["cases"]):
-#                 result = sandbox.run_command(["echo", "1"])
-#                 score = 100
-#                 time_ms = int(result.time * 1000)
-#                 memory_kb = result.memory
-#                 total_score += score
-#                 total_time_ms += time_ms
-#                 total_memory_kb += memory_kb
-#                 case_res = {
-#                     "index": i,
-#                     "result": {
-#                         "status": 1,
-#                         "score": score,
-#                         "time_ms": time_ms,
-#                         "memory_kb": memory_kb,
-#                         "execute_status": 1,
-#                         "stdout": result.stdout.read().decode(),
-#                         "stderr": result.stderr.read().decode(),
-#                     },
-#                 }
-#                 print(f"await case {i}")
-#                 async with session.post(
-#                     case_url, json=case_res, headers=HEADERS
-#                 ) as resp:
-#                     print(await resp.text())
-#         records_res = {
-#             "status": 1,
-#             "score": total_score,
-#             "time_ms": total_time_ms,
-#             "memory_kb": total_memory_kb,
-#             "judge_at": str(judge_at),
-#         }
-#         print("await record")
-#         async with session.post(record_url, json=records_res, headers=HEADERS) as resp:
-#             print(await resp.text())
-#
-#
+
+
 @app.task(name="joj.tiger.submit", bind=True)
 @async_command
 async def submit_task(
@@ -151,8 +102,23 @@ def add_task(self: Task, a: int, b: int) -> int:
     return a + b
 
 
+@retry_init("Celery")
+async def try_init_celery() -> None:
+    logger.info(app.control.inspect().active())
+
+
+def startup_event() -> None:  # pragma: no cover
+    try:
+        asyncio.run(try_init_celery())
+    except (RetryError) as e:
+        logger.error("Initialization failed, exiting.")
+        logger.error(e)
+        exit(-1)
+
+
 def main() -> None:
     toolchains_config.pull_images()
+    startup_event()
     argv = [
         "worker",
         f"--concurrency={settings.workers}",
